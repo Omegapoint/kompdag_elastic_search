0ac21e5443d7686829bc5750b34467102a254e5c
dansim <dana.simola@omegapoint.se>
2015-12-07 17:41:14 +0100

    Initial commit with contributors

---
+++ kompdag/src/main/java/se/omegapoint/KompdagApplication.java
@@ -0,0 +1,12 @@
+package se.omegapoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KompdagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KompdagApplication.class, args);
+    }
+}
